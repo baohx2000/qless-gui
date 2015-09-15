1888edc85f28d71a44b37fca6f2758e470981fbd
Gordon <baohx2000@gmail.com>
2015-09-15 07:15:41 -0700

    remove old js app

--- js_src/redux/qless/qless-constants.js
+++
@@ -1,7 +0,0 @@
-export const GET_QUEUES = 'GET_QUEUES';
-export const GET_JOBS = 'GET_JOBS';
-export const GET_WORKERS = 'GET_WORKERS';
-export const GET_JOB = 'GET_JOB';
-export const CANCEL_JOB = 'CANCEL_JOB';
-export const REQUEUE_JOB = 'REQUEUE_JOB';
-
--- js_src/redux/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import qless from './qless';
-
-const rootReducer = combineReducers({
-    qless
-});
-
-export default rootReducer;
