2230a12688515a90a665b06930466e79f3d2b233
Gordon Forsythe <baohx2000@gmail.com>
2015-11-10 12:06:17 -0700

    change rendering of index

--- router.php
+++ router.php
@@ -3,4 +3,4 @@ $url = parse_url($_SERVER['REQUEST_URI']);
 if (preg_match('/\.(?:js|css|html|php|handlebars|png|gif|jpg)$/', $url['path'])) {
     return false;    // serve the requested resource as-is.
 }
-echo file_get_contents(__DIR__.'/public/index.html');
+include __DIR__.'/public/index.html';
